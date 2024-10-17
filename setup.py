fb938a34783d3262962293ea9d5396f09b6c1193
Hari Krishna <hari10599@gmail.com>
2024-10-17 16:16:20 +0530

    add setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zd-keyczar",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Hari",
+    author_email="harikrishna@zerodown.com",
+    description="Keyczar",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zerodownhome/zd-keyczar",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
